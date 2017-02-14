9519be3a855fda1e06e0246c061b76c35da6a8f6
PaulDok <pauldok@rambler.ru>
2017-02-14 22:28:59 +0300

    Оживить интерактивные элементы в личном проекте
    - карта отцентрирована, ширина=контейнеру (1200px); блок с адресом не
    двигал т.к. по макету он как раз там где надо;
    - всем кнопкам (button) прописаны type;
    - у элементов с float удалены display: inline-block и vertical-align;
    - у всех inline-block прописан vertical-align;
    - height: 100% у .slider и самих слайдов;
    - карта с правильным пином, при своей инициализации скрывает
    плейсхолдер-картинку

--- js/script.js
+++ js/script.js
@@ -18,3 +18,37 @@ var hideFeedbackOverlay = function (evt) {
 
 feedbackOpenButton.addEventListener('click', showFeedbackOverlay);
 feedbackOverlay.addEventListener('click', hideFeedbackOverlay);
+
+
+// Map
+var mapPlaceholder = document.querySelector('#map-img');
+var pinPlaceholder = document.querySelector('#map-pin');
+
+ymaps.ready(init);
+var myMap;
+var myPlacemark;
+
+function init () {
+  myMap = new ymaps.Map("map", {
+    center: [59.93933268, 30.32872051],
+    zoom: 16,
+    controls: []
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+  myPlacemark = new ymaps.Placemark([59.93861704, 30.32286398], {
+    hintContent: 'Gllacy'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/map-pin.png',
+    iconImageSize: [218, 142],
+    iconImageOffset: [-40, -135]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+
+  myMap.behaviors.disable('scrollZoom');
+
+  mapPlaceholder.hidden = true;
+  pinPlaceholder.hidden = true;
+};
